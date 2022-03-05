f1546a8a02e969a1c33dcbec4577ae33647f76db
Mateusz Gąsiorek <fikserfredpl@gmail.com>
2022-03-05 21:47:29 +0100

    Added comments

--- test1/Program.cs
+++ test1/Program.cs
@@ -5,13 +5,13 @@ class main
     public static void Main()
     {
         int variaw = 0; //var controlling 
-        int day, hour, minute;
-        TimeSpan ts;
-        DateTime newDate;
+        int day, hour, minute; //variables for adding abd substracting
+        TimeSpan ts; //TimeSpan object
+        DateTime newDate; 
         DateTime Date;
         do
         {
-            Date = DateTime.Now;
+            Date = DateTime.Now;//Date refreshes every time do..while ends its iteration
             Console.WriteLine("What do you want to do?");
             Console.WriteLine("1. Display current time and date");
             Console.WriteLine("2. Add to date");
@@ -20,13 +20,13 @@ class main
             variaw = Convert.ToInt32(Console.ReadLine());
             switch (variaw)
             {
-                case 1:
+                case 1://Displaying current date and time
                     Console.Clear();
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine($"Date now: {Date} \n");
                     Console.ResetColor();
                     break;
-                case 2:
+                case 2://Adds to Date
                     Console.Clear();
                     Console.Write("Days: ");
                     day = Convert.ToInt32(Console.ReadLine());
@@ -39,10 +39,9 @@ class main
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine($"{newDate}\n");
                     Console.ResetColor();
-
                     break;
-                case 3:
-                    Console.Clear();                
+                case 3://Substracts from Date
+                    Console.Clear();
                     Console.Write("Days: ");
                     day = Convert.ToInt32(Console.ReadLine());
                     Console.Write("Hours: ");
@@ -55,9 +54,9 @@ class main
                     Console.WriteLine($"{newDate}\n");
                     Console.ResetColor();
                     break;
-                default:                    
+                default:
                     break;
             }
-        } while (variaw!= 0) ;
+        } while (variaw != 0);//Gives an exception when enter is pressed and nothing is in the buffer
     }
 }
